35c4476a05119faaa3e2978684d1bff4c9e26e92
don-c-smith <don.cs.smith@gmail.com>
2025-05-06 09:52:19 -0500

    Wrote comment skeleton for program

--- basic_ollama.py
+++ basic_ollama.py
@@ -1,1 +1,13 @@
 # Basic Ollama implementation
+
+# Import statements - Ollama, langchain, etc
+
+# Function to select a local LLM
+
+# Function to decide if we're going to cast the LLM as an 'expert' of a specified type
+
+# Function to choose a response style - either one of pre-defined options or free-entry
+
+# Function to provide the actual prompt
+
+# Function to send the langchain call to the LLM and provide a response
