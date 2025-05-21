fe39dea1e6f122730ca8c1c8b99a6fe38056cc18
don-c-smith <don.cs.smith@gmail.com>
2025-05-21 13:25:42 -0500

    Uploaded skeleton for new LLM text reader

---
+++ text_summarizer.py
@@ -0,0 +1,9 @@
+# Import the tkinter, Ollama, and langchain libraries, along with anything else we might need
+
+# Use tkinter to build a simple GUI where a user can upload a text file
+
+# Set up a query to the local gemma3:12b library which asks it to read the text file and create a summary
+# The summary should start with five bullet points which are the most important points/elements in the text
+# The summary should end with a short paragraph (of ~250 words) which is a more detailed summary of the text
+
+# Finally, the user should be offered the opportunity to save the summary to a local file, again using a simple tkinter GUI
